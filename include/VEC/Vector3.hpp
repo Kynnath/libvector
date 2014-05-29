ffd47446daed02979e93a45d1d8dd02e0b65e89f
juan.garibotti <juan.garibotti@baiwks0171.bai.gameloft.org>
2014-05-29 15:20:36 -0300

    - Changed return values from methods so they can act similar to the assignment operations. If it's good enough for int, it's good enough for me.

--- include/VEC/Vector3.hpp
+++ include/VEC/Vector3.hpp
@@ -19,14 +19,14 @@ namespace vec
         Vector3() = default;
         double & operator[]( int const& i_arrayIndex );
         double const& operator[]( int const& i_arrayIndex ) const;
-        void Add( Vector3 const& i_rhs );
-        void Subtract( Vector3 const& i_rhs );
-        void Scale( double const& i_scaleFactor );
+        Vector3 & Add( Vector3 const& i_rhs );
+        Vector3 & Subtract( Vector3 const& i_rhs );
+        Vector3 & Scale( double const& i_scaleFactor );
         double DotProduct( Vector3 const& i_rhs ) const;
         double MagnitudeSquared() const;
         double Magnitude() const;
         bool Equals( Vector3 const& i_rhs ) const;
-        void Normalize();
+        Vector3 & Normalize();
     };
 
     Vector3 Add( Vector3 c_lhs, Vector3 const& i_rhs );
--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -25,25 +25,28 @@ namespace vec
         return m_data[ (long long unsigned int)i_arrayIndex ];
     }
 
-    void Vector3::Add( Vector3 const& i_rhs )
+    Vector3 & Vector3::Add( Vector3 const& i_rhs )
     {
         m_data[ 0 ] += i_rhs[ 0 ];
         m_data[ 1 ] += i_rhs[ 1 ];
         m_data[ 2 ] += i_rhs[ 2 ];
+        return *this;
     }
 
-    void Vector3::Subtract( Vector3 const& i_rhs )
+    Vector3 & Vector3::Subtract( Vector3 const& i_rhs )
     {
         m_data[ 0 ] -= i_rhs[ 0 ];
         m_data[ 1 ] -= i_rhs[ 1 ];
         m_data[ 2 ] -= i_rhs[ 2 ];
+        return *this;
     }
 
-    void Vector3::Scale( double const& i_scaleFactor )
+    Vector3 & Vector3::Scale( double const& i_scaleFactor )
     {
         m_data[ 0 ] *= i_scaleFactor;
         m_data[ 1 ] *= i_scaleFactor;
         m_data[ 2 ] *= i_scaleFactor;
+        return *this;
     }
 
     double Vector3::DotProduct( Vector3 const& i_rhs ) const
@@ -63,10 +66,10 @@ namespace vec
 
     bool Vector3::Equals( Vector3 const& i_rhs ) const
     {
-        return ( !memcmp( this, &i_rhs, sizeof( Vector3 ) ) );
+        return ( memcmp( this, &i_rhs, sizeof( Vector3 ) ) == 0 );
     }
 
-    void Vector3::Normalize()
+    Vector3 & Vector3::Normalize()
     {
         double const magnitude ( Magnitude() );
 
@@ -75,24 +78,23 @@ namespace vec
         m_data[ 0 ] /= magnitude;
         m_data[ 1 ] /= magnitude;
         m_data[ 2 ] /= magnitude;
+
+        return *this;
     }
 
     Vector3 Add( Vector3 c_lhs, Vector3 const& i_rhs )
     {
-        c_lhs.Add( i_rhs );
-        return c_lhs;
+        return c_lhs.Add( i_rhs );
     }
 
     Vector3 Subtract( Vector3 c_lhs, Vector3 const& i_rhs )
     {
-        c_lhs.Subtract( i_rhs );
-        return c_lhs;
+        return c_lhs.Subtract( i_rhs );
     }
 
     Vector3 Scale( Vector3 c_lhs, double const& i_rhs )
     {
-        c_lhs.Scale( i_rhs );
-        return c_lhs;
+        return c_lhs.Scale( i_rhs );
     }
 
     double DotProduct( Vector3 const& i_lhs, Vector3 const& i_rhs )
@@ -116,14 +118,11 @@ namespace vec
         double const yComponent ( i_lhs[ 2 ] * i_rhs[ 0 ] - i_lhs[ 0 ] * i_rhs[ 2 ] );
         double const zComponent ( i_lhs[ 0 ] * i_rhs[ 1 ] - i_lhs[ 1 ] * i_rhs[ 0 ] );
 
-        Vector3 const crossProduct = { { xComponent, yComponent, zComponent } };
-
-        return crossProduct;
+        return Vector3 { { xComponent, yComponent, zComponent } };
     }
 
     Vector3 Normalize( Vector3 c_vector )
     {
-        c_vector.Normalize();
-        return c_vector;
+        return c_vector.Normalize();
     }
 }
